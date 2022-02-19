01795a1a4ecd27ea9d4ce881bb556de7f69f81c5
VanessaMartinsG <82179516+VanessaMartinsG@users.noreply.github.com>
2022-02-19 00:37:44 -0300

    Refactor: change the functionality of the table icons

--- js/dashboard.js
+++ js/dashboard.js
@@ -2,42 +2,44 @@
 
 function abrirModalEditar(){
     const modal = document.querySelector(".modalEditar");
-    const editar = document.querySelector(".editar");
-    console.log(editar);
-
-editar.addEventListener("click", (e) => {
-    modal.style.display = "flex"; 
-});
+    const editar = document.querySelectorAll(".editar");
+    editar.forEach(item => {
+        item.addEventListener("click", (e) => {
+            modal.style.display = "flex"; 
+        });
+    });
 }
 
 function fecharModalEditar(){
     const modal = document.querySelector(".modalEditar");
-    const close = document.querySelector(".closeEditar");
-
-close.addEventListener("click", (e) => {
-    modal.style.display = "none"; 
-});
+    const close = document.querySelectorAll(".closeEditar");
+    close.forEach(item => {
+        item.addEventListener("click", (e) => {
+            modal.style.display = "none"; 
+        });
+    });
 }
 
 /* MODAL DE DELETAR */
 
 function abrirModalDeletar(){
     const modal = document.querySelector(".modalDeletar");
-    const deletar = document.querySelector(".deletar");
-    console.log(deletar);
-
-deletar.addEventListener("click", (e) => {
-    modal.style.display = "flex"; 
-});
+    const deletar = document.querySelectorAll(".deletar");
+    deletar.forEach(item => {
+        item.addEventListener("click", (e) => {
+            modal.style.display = "flex"; 
+        });
+    });
 }
 
 function fecharModalDeletar(){
     const modal = document.querySelector(".modalDeletar");
-    const close = document.querySelector(".cancelar");
-
-close.addEventListener("click", (e) => {
-    modal.style.display = "none"; 
-});
+    const close = document.querySelectorAll(".cancelar");
+    close.forEach(item => {
+        item.addEventListener("click", (e) => {
+            modal.style.display = "none"; 
+        });
+    });
 }
 
 
